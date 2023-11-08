b0a79ca1ef5e9335095a43aaf809329d52ae1f4a
maershaa <valeria.work2022@gmail.com>
2023-11-08 23:50:46 +0200

    2 commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
@@ -7,132 +7,119 @@ import Modal from './Modal/Modal';
 
 import fetchPhotos from '../helpers/api';
 
-class App extends Component {
-  state = {
-    photos: null, // Состояние для хранения фотографий
-    isLoading: false, // Состояние для отображения индикатора загрузки
-    error: null, // Состояние для хранения сообщения об ошибке (если есть)
-    inputValue: '', // Состояние для хранения поискового запроса
-    currentPage: 1, //  состояние для текущей страницы
-    selectedPhoto: null, //  состояние для выбранного изображения
+const App = () => {
+  const [photos, setPhotos] = useState(null); // Состояние для хранения фотографий
+  const [isLoading, setIsLoading] = useState(false); // Состояние для отображения индикатора загрузки
+  const [error, setError] = useState(null); // Состояние для хранения сообщения об ошибке (если есть)
+  const [inputValue, setInputValue] = useState(''); // Состояние для хранения поискового запроса
+  const [currentPage, setCurrentPage] = useState(1); // Состояние для текущей страницы
+  const [selectedPhoto, setSelectedPhoto] = useState(null); // Состояние для выбранного изображения
+
+  // Функция для обработки отправки поискового запроса
+  const handleSubmit = query => {
+    setPhotos([]); // Очищаем фотографии перед новым поиском
+    setInputValue(query); // Устанавливаем значение поискового запроса
+    setCurrentPage(1); // Сбрасываем текущую страницу при новом поиске
   };
 
-  // Обработчик изменения значения поискового запроса
-
-  handleSubmit = query => {
-    this.setState({ photos: [] });
-    this.setState({ inputValue: query, currentPage: 1 });
-  };
-
-  clearPhotos = () => {
-    this.setState({ photos: [] });
+  // Функция для очистки фотографий
+  const clearPhotos = () => {
+    setPhotos([]); // Очищаем список фотографий
   };
 
   // Функция для выполнения запроса и обновления фотографий
-  async fetchAndSetPhotos(inputValue, currentPage = 1) {
+  const fetchAndSetPhotos = async (inputValue, currentPage = 1) => {
     try {
-      // Устанавливаем isLoading в true перед началом запроса
-      this.setState({ isLoading: true, error: null });
+      setIsLoading(true); // Устанавливаем флаг загрузки в true
+      setError(null); // Сбрасываем сообщение об ошибке
 
-      // Выполняем запрос к API с переданным поисковым запросом и номером страницы
-      const data = await fetchPhotos(inputValue, currentPage);
+      const data = await fetchPhotos(inputValue, currentPage); // Выполняем запрос к API
 
-      // При успешном запросе, добавляем новые фотографии к текущим
-      const currentPhotos = this.state.photos || []; // Существующие фотографии
-      const newPhotos = data.hits; // Новые фотографии
+      const currentPhotos = photos || []; // Получаем текущие фотографии
+      const newPhotos = data.hits; // Получаем новые фотографии
 
       const updatedPhotos = [...currentPhotos, ...newPhotos]; // Объединяем их
 
-      this.setState({ photos: updatedPhotos, error: null });
+      setPhotos(updatedPhotos); // Устанавливаем обновленные фотографии
+      setError(null); // Сбрасываем сообщение об ошибке
     } catch (error) {
-      // В случае ошибки сохраняем сообщение об ошибке и очищаем фотографии
-      this.setState({ error: error.message, photos: null });
+      setError(error.message); // Устанавливаем сообщение об ошибке
+      setPhotos(null); // Сбрасываем фотографии в случае ошибки
     } finally {
-      // В любом случае завершаем запрос, сбрасывая флаг isLoading
-      this.setState({ isLoading: false });
+      setIsLoading(false); // В любом случае завершаем запрос, сбрасывая флаг загрузки
     }
-  }
+  };
 
-  componentDidUpdate(_, prevState) {
-    const { inputValue, currentPage } = this.state;
-    if (
-      prevState.inputValue !== this.state.inputValue ||
-      prevState.currentPage !== this.state.currentPage
-    ) {
-      this.fetchAndSetPhotos(inputValue, currentPage);
-      // Очистить список фотографий перед выполнением нового запроса
-      this.clearPhotos();
+  // Эффект для выполнения запроса и обновления фотографий при изменении inputValue или currentPage
+  useEffect(() => {
+    // Проверяем, что inputValue и currentPage не пусты перед выполнением запроса
+    if (inputValue && currentPage) {
+      fetchAndSetPhotos(inputValue, currentPage);
+      clearPhotos(); // Очищаем список фотографий перед выполнением нового запроса
     }
-  }
+  }, [inputValue, currentPage]);
 
   // Обработчик для кнопки "Загрузить еще"
-  loadMoreImages = () => {
-    // Увеличиваем текущую страницу на 1
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    })); // Обновляем текущую страницу
+  const loadMoreImages = () => {
+    setCurrentPage(currentPage + 1); // Увеличиваем текущую страницу на 1
   };
 
   // Обработчик клика на изображение в галерее
-  handleImageClick = selectedPhoto => {
-    this.setState({ selectedPhoto });
+  const handleImageClick = selectedPhoto => {
+    setSelectedPhoto(selectedPhoto); // Устанавливаем выбранное изображение
   };
 
   // Метод для закрытия модального окна
-  closeModal = () => {
-    console.log('Closing modal');
-    this.setState({ selectedPhoto: null });
+  const closeModal = () => {
+    console.log('Closing modal'); // Выводим сообщение в консоль
+    setSelectedPhoto(null); // Закрываем модальное окно
   };
 
-  render() {
-    // Деструктуризация для упрощения доступа к состояниям
-    const { error, isLoading, photos, selectedPhoto } = this.state;
-    const noPhotos = photos && photos.length === 0;
+  // Проверка на отсутствие фотографий
+  const noPhotos = !isLoading && photos && photos.length === 0;
 
-    return (
-      <>
-        {/* Компонент поисковой строки с передачей обработчиков событий */}
-        <Searchbar onSearch={this.handleSubmit} />
+  return (
+    <>
+      {/* Компонент поисковой строки с передачей обработчиков событий */}
+      <Searchbar onSearch={handleSubmit} />
 
-        {/* Отображаем сообщение об ошибке, если ошибка не равна null */}
-        {error !== null && (
-          <p className="errorBage">
-            Oops, some error occurred... Error message: {error}
-          </p>
-        )}
+      {/* Отображаем сообщение об ошибке, если ошибка не равна null */}
+      {error !== null && (
+        <p className="errorBage">
+          Oops, some error occurred... Error message: {error}
+        </p>
+      )}
 
-        {/* Отображаем индикатор загрузки, если isLoading равно true */}
-        {isLoading && <Loader />}
+      {/* Отображаем индикатор загрузки, если isLoading равно true */}
+      {isLoading && <Loader />}
 
-        {/* Если переменная noPhotos имеет значение true, отображаем сообщение об извинении, что не найдено изображений по вашему запросу */}
-        {noPhotos && !isLoading && (
-          <p className="apologyMessage">
-            Sorry, no images were found for your search.
-          </p>
-        )}
+      {/* Если переменная noPhotos имеет значение true, отображаем сообщение об извинении, что не найдено изображений по вашему запросу */}
+      {noPhotos && !isLoading && (
+        <p className="apologyMessage">
+          Sorry, no images were found for your search.
+        </p>
+      )}
 
-        {/* Отображаем галерею изображений, если фотографии не равны null и их количество больше 0 */}
-        {photos && photos.length > 0 && (
-          <>
-            <ImageGallery photos={photos} onClick={this.handleImageClick} />
+      {/* Отображаем галерею изображений, если фотографии не равны null и их количество больше 0 */}
+      {photos && photos.length > 0 && (
+        <>
+          <ImageGallery photos={photos} onClick={handleImageClick} />
 
-            {/* скрывает кнопку "Загрузить еще", если количество полученных изображений не делится на 12 (т.е. остаток от деления не равен нулю). */}
-            {photos && photos.length > 0 && photos.length % 12 === 0 && (
-              <Button
-                photos={photos}
-                isLoading={isLoading}
-                onLoadMore={this.loadMoreImages}
-              />
-            )}
-          </>
-        )}
+          {/* Скрываем кнопку "Загрузить еще", если количество полученных изображений не делится на 12 (т.е. остаток от деления не равен нулю). */}
+          {photos && photos.length > 0 && photos.length % 12 === 0 && (
+            <Button
+              photos={photos}
+              isLoading={isLoading}
+              onLoadMore={loadMoreImages}
+            />
+          )}
+        </>
+      )}
 
-        {selectedPhoto && ( // Если есть выбранное изображение, отображаем модальное окно
-          <Modal modalData={selectedPhoto} closeModal={this.closeModal} />
-        )}
-      </>
-    );
-  }
-}
-
+      {selectedPhoto && ( // Если есть выбранное изображение, отображаем модальное окно
+        <Modal modalData={selectedPhoto} closeModal={closeModal} />
+      )}
+    </>
+  );
+};
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,52 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    // Добавляем обработчик события нажатия клавиши "Esc" при монтировании компонента
-    window.addEventListener('keydown', this.handleKeyDown);
-    // Запрещаем прокрутку тела документа при открытом модальном окне
-    document.body.style.overflow = 'hidden';
-  }
-
-  componentWillUnmount() {
-    // Удаляем обработчик события при размонтировании компонента
-    window.removeEventListener('keydown', this.handleKeyDown);
-    // Восстанавливаем прокрутку тела документа при закрытом модальном окне
-    document.body.style.overflow = 'auto';
-  }
-
-  handleOverlayClick = event => {
+const Modal = ({ modalData, closeModal }) => {
+  const handleOverlayClick = event => {
     // Проверяем, было ли событие клика на самом оверлее, а не на его дочерних элементах
     if (event.target === event.currentTarget) {
       // Вызываем функцию closeModal переданную через props
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  handleKeyDown = event => {
-    // Проверяем, была ли нажата клавиша "Esc"
-    if (event.code === 'Escape') {
-      // Вызываем функцию closeModal переданную через props
-      this.props.closeModal();
-    }
-  };
+  // componentDidMount()
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    };
 
-  render() {
-    // Извлекаем modalData из props
-    const { modalData } = this.props;
+    window.addEventListener('keydown', handleKeyDown);
+    document.body.style.overflow = 'hidden';
 
-    return (
-      // Добавляем обработчик клика на оверлее для закрытия модального окна
-      <div onClick={this.handleOverlayClick} className="overlay">
-        <div className="modal">
-          <img
-            // Отображаем увеличенное изображение с URL и альтернативным текстом из modalData
-            src={modalData.largeImageURL}
-            alt={modalData.tags}
-            className="modalImage"
-          />
-        </div>
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = 'auto';
+    };
+  }, [closeModal]);
+
+  return (
+    // Добавляем обработчик клика на оверлее для закрытия модального окна
+    <div onClick={handleOverlayClick} className="overlay">
+      <div className="modal">
+        <img
+          // Отображаем увеличенное изображение с URL и альтернативным текстом из modalData
+          src={modalData.largeImageURL}
+          alt={modalData.tags}
+          className="modalImage"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Modal;
